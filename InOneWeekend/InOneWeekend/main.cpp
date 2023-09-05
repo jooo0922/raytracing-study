de1faaa055276b0baed4cadce9966b4b8f2e98f6
jooo0922 <jooo0922@gmail.com>
2023-09-05 22:01:55 +0900

    feat: 색상 출력 progress indicator 구현

--- InOneWeekend/InOneWeekend/main.cpp
+++ InOneWeekend/InOneWeekend/main.cpp
@@ -15,6 +15,10 @@ int main()
 	// 각 픽셀의 색상값(r, g, b)을 0 ~ 256 사이의 scale 된 정수값 범위로 출력
 	for (int j = 0; j < image_height; ++j)
 	{
+		// ppm 에 출력할 색상값을 한 줄(row)씩 처리할 때마다 남아있는 줄을 std::clog 로 출력함 > 에러 출력 스트림
+		// 참고로, std::flush 는 스트림에 대기중인 버퍼의 내용을 비운 후, 강제로 출력시킴.
+		// std::cout 은 색상값 출력 시 사용되므로, 이에 대한 대체제로써 std::clog 를 사용한 것!
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
 			// 관례적으로, r, g, b 값은 0 ~ 1 사이의 범위의 실수형으로 맵핑해서 계산해놓음.
@@ -31,4 +35,6 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::clog << "\rDone.					\n"; // 반복문이 종료되면 .ppm 에 출력할 색상 계산이 완료되었음을 std::clog 로 콘솔 출력함.
 }
